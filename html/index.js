ee91e0718ff7917486c5a9b8a6ead1cce86b38a9
arya230 <aryangairola@gmail.com>
2022-05-28 12:28:54 +0530

    javascript revision started

---
+++ html/index.js
@@ -0,0 +1,42 @@
+// // Starting JS-> Javascript
+// // variable declaration
+// var a;
+// //variable intialization
+// a=10;
+// console.log(typeof a);
+// console.log(a);
+
+// a = "hello";
+// console.log(a);
+
+// console.log(typeof a);
+
+// a = true;
+// console.log(a);
+// console.log(typeof a);
+
+// a = null;
+// console.log(a);
+// console.log(typeof a);
+
+
+var num;
+num = 10;
+console.log(num);
+var float;
+float = 2.4;
+console.log(float);
+// '',"",``
+//single quotes, double quotes and backtick
+
+var str = 'a';
+console.log(typeof str);
+str = "how you doin ?"
+console.log(str);
+
+//var has some problem
+var b  = `I expect myself to 
+grasp the concepts`;
+console.log(b);
+var num = 3450;
+console.log(`half of ${num} is ${num/2}`);
