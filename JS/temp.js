0813e571b214ba436fddf8cfcef596d6a717316f
arya230 <aryangairola@gmail.com>
2022-09-17 18:59:51 +0530

    fs module

---
+++ JS/temp.js
@@ -0,0 +1,11 @@
+var obj = {
+    add:function add(a, b) {
+        return a + b;
+    },
+    sub:function subtract(a, b) {
+        return a - b;
+    },
+    hello:123
+}
+console.log(obj);
+module.exports = obj;
--- node/fs.js
+++ node/fs.js
@@ -1,12 +1,17 @@
 // fs-> stands file system module --  it helps too create file , folders ,put data in them, delete data and read them
 //appendFileSync appends data into the file, if not present, it creates the file and then appends the data
-const fs = require("fs");
+const fs = require("fs");//require method, goes to the file that is needed to be required , execute that file and if there is something returned/exported we get that in variable 'abc'
+const { add } = require("../JS/temp");
 console.log(fs);
-let res = fs.appendFileSync("f1.txt", "Hello I am f1 file");
-fs.appendFileSync("f1.txt", "\nyou guys are smart");
+const abc = require("../JS/temp")
+console.log(abc);
+let ans = add(2, 4);
+console.log(ans);
+// let res = fs.appendFileSync("f1.txt", "Hello I am f1 file");
+// fs.appendFileSync("f1.txt", "\nyou guys are smart");
 
-console.log(res);
-// let data = fs.readFileSync("f1.txt",);
-// console.log(data + "");
-let data = fs.readFileSync("f1.txt", "utf-8");
-console.log(data);
+// console.log(res);
+// // let data = fs.readFileSync("f1.txt",);
+// // console.log(data + "");
+// let data = fs.readFileSync("f1.txt", "utf-8");
+// console.log(data);
