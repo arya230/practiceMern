41fa86e79e1d82a5076dceaaa780e4f85ffe027b
arya230 <aryangairola@gmail.com>
2022-09-17 12:19:39 +0530

    closures

--- JS/closures.js
+++ JS/closures.js
@@ -8,14 +8,26 @@
 // }
 // outer();
 
+// function outer() {
+//     var a = 10;
+//     function inner() {
+//         console.log(a);
+//     }
+//     return inner;
+
+// }
+// var z = outer();
+// console.log(z + "");
+// z();
+
 function outer() {
-    var a = 10;
+    var a = 7;
     function inner() {
         console.log(a);
     }
+    a = 10;
     return inner;
-
 }
-var z = outer();
-console.log(z + "");
-z();
+ var c = outer();
+ console.log(c);
+ c();
